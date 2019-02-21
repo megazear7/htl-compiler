225be889d111b59369ca16edd38c5afc68154126
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-21 12:10:14 -0500

    Created basic structure for compiling interpreted htl.

--- index.js
+++ index.js
@@ -1,1 +1,215 @@
 console.log('hello, world');
+
+
+class Args {
+  constructor({_hasParens, _map}) {
+    this.hasParens = _hasParens;
+    this._map = _map;
+  }
+}
+
+class Expression {
+  constructor({_hasParens, _functionName, _args, _expression}) {
+    this.hasParens = _hasParens;
+    this.functionName = _functionName;
+    this.args = _args;
+
+    if (_expression) {
+      this.expression = new Expression(_expression);
+    }
+  }
+
+  get output() {
+    return ' -expression- ';
+  }
+}
+
+class Token {
+  constructor({_variableName, _text, _location, _expression}) {
+    this.variableName = _variableName;
+    this.text = _text;
+    this.location = _location;
+
+    if (_expression) {
+      this.expression = new Expression(_expression);
+    }
+  }
+
+  get output() {
+    return ' -token- ';
+  }
+}
+
+class TokenList {
+  constructor(tokenArray) {
+    this.tokens = tokenArray.map(token => new Token(token));
+  }
+
+  get output() {
+    return this.tokens.reduce((outputHtml, token) => outputHtml + token.output, '');
+  }
+}
+
+const tokenArray = [
+  {
+		"_variableName": "headerComponent",
+		"_expression": {
+			"_hasParens": false,
+			"_functionName": "use",
+			"_expression": {
+				"_hasParens": false,
+				"_text": "some.path.to.a.java.ExampleClass"
+			},
+			"_args": [{
+				"_hasParens": false,
+				"_map": {
+
+				}
+			}]
+		}
+	},
+	{
+		"_expression": {
+			"_hasParens": false,
+			"_value": false,
+			"_text": false
+		}
+	},
+	{
+		"_text": "<sly>"
+	},
+	{
+
+	},
+	{
+		"_expression": {
+			"_hasParens": false,
+			"_value": false,
+			"_text": false
+		}
+	},
+	{
+		"_text": "</sly>"
+	},
+	{
+
+	},
+	{
+		"_text": "\n\n<h1>"
+	},
+	{
+		"_variableName": "var_0",
+		"_expression": {
+			"_hasParens": false,
+			"_target": {
+				"_hasParens": false,
+				"_name": "headerComponent"
+			},
+			"_property": {
+				"_hasParens": false,
+				"_text": "title"
+			}
+		}
+	},
+	{
+		"_location": {
+			"line": 2,
+			"column": 4
+		},
+		"_variableName": "var_0"
+	},
+	{
+
+	},
+	{
+		"_text": "</h1>\n<p>"
+	},
+	{
+		"_variableName": "var_1",
+		"_expression": {
+			"_hasParens": false,
+			"_target": {
+				"_hasParens": false,
+				"_name": "headerComponent"
+			},
+			"_property": {
+				"_hasParens": false,
+				"_text": "description"
+			}
+		}
+	},
+	{
+		"_location": {
+			"line": 3,
+			"column": 3
+		},
+		"_variableName": "var_1"
+	},
+	{
+
+	},
+	{
+		"_text": "</p>\n\n"
+	},
+	{
+		"_expression": {
+			"_hasParens": false,
+			"_value": false,
+			"_text": false
+		}
+	},
+	{
+		"_text": "<sly>"
+	},
+	{
+
+	},
+	{
+		"_variableName": "var_resourceContent2",
+		"_expression": {
+			"_hasParens": false,
+			"_functionName": "resource",
+			"_expression": {
+				"_hasParens": false,
+				"_text": "foo.html"
+			},
+			"_args": [
+
+			]
+		}
+	},
+	{
+		"_variableName": "var_resourceContent2"
+	},
+	{
+
+	},
+	{
+		"_expression": {
+			"_hasParens": false,
+			"_value": false,
+			"_text": false
+		}
+	},
+	{
+
+	},
+	{
+		"_expression": {
+			"_hasParens": false,
+			"_value": false,
+			"_text": false
+		}
+	},
+	{
+		"_text": "</sly>"
+	},
+	{
+
+	},
+	{
+		"_text": "\n"
+	}
+];
+
+console.log((new TokenList(tokenArray)).output);
