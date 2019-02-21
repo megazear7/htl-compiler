076364da543c20a3682c2abe27013798d18e60d6
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-21 12:50:22 -0500

    Rendering variables.

--- index.js
+++ index.js
@@ -1,52 +1,97 @@
-console.log('hello, world');
-
+const FUNCTION_USE = 'use';
 
 class Args {
   constructor({_hasParens, _map}) {
     this.hasParens = _hasParens;
-    this._map = _map;
+    this.map = _map;
+  }
+}
+
+class Property {
+  constructor({_hasParens, _text}) {
+    this.hasParens = _hasParens;
+    this.text = _text;
+  }
+}
+
+class Target {
+  constructor({_hasParens, _name}) {
+    this.hasParens = _hasParens;
+    this.name = _name;
   }
 }
 
 class Expression {
-  constructor({_hasParens, _functionName, _args, _expression}) {
+  constructor({_hasParens, _functionName, _text, _target, _property, _args, _expression}, context) {
     this.hasParens = _hasParens;
     this.functionName = _functionName;
-    this.args = _args;
+    this.context = context;
+    this.text = _text;
 
-    if (_expression) {
-      this.expression = new Expression(_expression);
-    }
+    if (_target) this.target = new Target(_target);
+    if (_property) this.property = new Property(_property);
+    if (_args) this.args = new Args(_args);
+    if (_expression)this.expression = new Expression(_expression);
   }
 
   get output() {
-    return ' -expression- ';
+    if (this.target && this.property) {
+      return this.context[this.target.name][this.property.text];
+    } else {
+      return '';
+    }
   }
 }
 
 class Token {
-  constructor({_variableName, _text, _location, _expression}) {
+  constructor({_variableName, _text, _location, _expression}, context) {
     this.variableName = _variableName;
     this.text = _text;
     this.location = _location;
+    this.context = context;
 
-    if (_expression) {
-      this.expression = new Expression(_expression);
-    }
+    if (_expression) this.expression = new Expression(_expression, context);
   }
 
   get output() {
-    return ' -token- ';
+    if (this.text && this.text.indexOf('<sly') !== 0 && this.text.indexOf('</sly') !== 0) {
+      return this.text;
+    } else if (this.expression && this.expression.functionName === FUNCTION_USE) {
+      this.context[this.variableName] = this.context[this.expression.expression.text];
+      return '';
+    } else if (this.expression) {
+      return this.expression.output;
+    } else {
+      return '';
+    }
   }
 }
 
 class TokenList {
-  constructor(tokenArray) {
-    this.tokens = tokenArray.map(token => new Token(token));
+  constructor(tokenArray, context) {
+    this.tokens = tokenArray.map(token => new Token(token, context));
   }
 
   get output() {
     return this.tokens.reduce((outputHtml, token) => outputHtml + token.output, '');
+  }
+}
+
+class Compiler {
+  constructor(tokens, data) {
+    this.tokenList = new TokenList(tokens, data, {});
+  }
+
+  compile() {
+    return this.tokenList.output;
+  }
+}
+
+const exampleData = {
+  "exampleGlobalValue": "Example global value",
+  "some.path.to.a.java.ExampleClass": {
+    "title": "Example title",
+    "description": "Example description"
   }
 }
 
@@ -212,4 +257,4 @@ const tokenArray = [
 	}
 ];
 
-console.log((new TokenList(tokenArray)).output);
+console.log((new Compiler(tokenArray, exampleData)).compile());
