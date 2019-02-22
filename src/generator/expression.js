0ce95c8df0df24a0fcbd60e6876eea1bcf1f09ad
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-22 14:26:35 -0500

    Global variables working.

--- src/generator/expression.js
+++ src/generator/expression.js
@@ -3,11 +3,12 @@ import Property from './property.js';
 import Args from './args.js';
 
 export default class Expression {
-  constructor({_hasParens, _functionName, _text, _target, _property, _args, _expression}, context) {
+  constructor({_hasParens, _functionName, _text, _name, _target, _property, _args, _expression}, context) {
     this.hasParens = _hasParens;
     this.functionName = _functionName;
     this.context = context;
     this.text = _text;
+    this.name = _name;
 
     if (_target) this.target = new Target(_target);
     if (_property) this.property = new Property(_property);
@@ -18,6 +19,8 @@ export default class Expression {
   get output() {
     if (this.target && this.property) {
       return this.context[this.target.name][this.property.text];
+    } else if (this.name) {
+      return this.context[this.name];
     } else {
       return '';
     }
