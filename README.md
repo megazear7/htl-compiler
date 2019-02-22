eeddd07e4e6c62b5b460de314f5af140922127fe
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-22 12:16:27 -0500

    Added a test generator and used it to add a test.

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-Create test cases for the following:
-${expression}
-<sly>
-data-sly-use
-data-sly-text
-data-sly-attribute
-data-sly-element
-data-sly-test
-data-sly-list
-data-sly-repeat
-data-sly-include
-data-sly-call
-data-sly-unwrap
-data-sly-set
+# HTL Compiler
+
+## Test
+
+```
+npm test
+```
+
+#### Create new empty test
+
+```
+npm run test:create
+```
+
+## TODO
+
+Create tests for, and then implement, the following HTL features:
+
+1. ${expression}
+1. <sly>
+1. data-sly-use
+1. data-sly-text
+1. data-sly-attribute
+1. data-sly-element
+1. data-sly-test
+1. data-sly-list
+1. data-sly-repeat
+1. data-sly-include
+1. data-sly-call
+1. data-sly-unwrap
+1. data-sly-set
---
+++ create.js
@@ -0,0 +1,72 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const newTestNumber = fs.readdirSync(path.resolve(__dirname, `./test/cases`))
+.map(fileName => fileName.split("test-")[1])
+.map(fileName => fileName.split(".json")[0])
+.map(testNumber => parseInt(testNumber))
+.reduce((a, b) => Math.max(a, b)) + 1;
+
+console.log(newTestNumber);
+
+const fileName = `test-${newTestNumber}`;
+
+fs.writeFile(
+  path.resolve(__dirname, `./test/cases/${fileName}.json`), `{
+  "template": "example-test.html",
+  "it": "Should match the output"
+}`,
+  'utf-8'
+);
+
+fs.writeFile(
+  path.resolve(__dirname, `./test/resource-data/${fileName}.json`), `{
+  "exampleGlobalValue": "Example global value"
+}
+`,
+  'utf-8'
+);
+
+fs.writeFile(
+  path.resolve(__dirname, `./test/use-models/${fileName}.json`), `{
+  "some.path.to.a.java.ExampleClass":
+    {
+      "title": "Example title",
+      "description": "Example description"
+    },
+  "some.path.to.a.java.FooClass":
+    {
+      "example": "I am from FooClass"
+    }
+}
+`,
+  'utf-8'
+);
+
+fs.writeFile(
+  path.resolve(__dirname, `./test/resources/${fileName}.json`), `{
+  "foo": "foo-resource.html"
+}
+`
+,
+  'utf-8'
+);
+
+fs.writeFile(
+  path.resolve(__dirname, `./test/expected/${fileName}.html`), `
+<p>Example Test</p>
+
+
+
+<h1>Example title</h1>
+<p>Example description</p>
+
+<p>Example resource</p>
+
+
+
+<div>This is the foo resource. It has its own scope.</div>
+<div>I am from FooClass</div>
+`,
+  'utf-8'
+);
