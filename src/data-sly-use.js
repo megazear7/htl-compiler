b90433e54255cae7b6e7c9b594d63e0c01570719
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-07-09 17:03:11 -0400

    Updated use model to work without a HTL expression

--- src/data-sly-use.js
+++ src/data-sly-use.js
@@ -13,10 +13,12 @@ export default class DataSlyUse {
     const handle = this.name.split('\.')[1];
     const matches = expressionMatch.exec(this.value);
 
+    let classPath = this.value;
     if (matches && matches.length >= 1) {
-      const classPath = matches[1];
-      this.compiler.resourceData[handle] = this.compiler.useModels[classPath];
+      classPath = matches[1];
     }
+
+    this.compiler.resourceData[handle] = this.compiler.useModels[classPath];
 
     return output;
   }
